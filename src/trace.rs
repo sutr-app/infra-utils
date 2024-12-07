61d90bcb0b71c5c56c5603bacd88240c66d80bf5
sutr <91235033+sutr-app@users.noreply.github.com>
2024-12-07 15:55:09 +0900

    fix clippy

--- src/trace.rs
+++ src/trace.rs
@@ -10,7 +10,7 @@ use tonic::Request;
 
 struct MetadataMap<'a>(&'a tonic::metadata::MetadataMap);
 
-impl<'a> Extractor for MetadataMap<'a> {
+impl Extractor for MetadataMap<'_> {
     /// Get a value for a key from the MetadataMap.  If the value can't be converted to &str, returns None
     fn get(&self, key: &str) -> Option<&str> {
         self.0.get(key).and_then(|metadata| metadata.to_str().ok())
