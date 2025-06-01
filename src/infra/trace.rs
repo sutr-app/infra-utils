132d1c2b8a93053387a826ecfa62cc7f04eede8a
sutr <91235033+sutr-app@users.noreply.github.com>
2025-06-01 14:10:12 +0900

    fix lint

--- src/infra/trace.rs
+++ src/infra/trace.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
-use opentelemetry::global::BoxedSpan;
 use opentelemetry::propagation::Injector;
-use opentelemetry::trace::{SpanKind, SpanRef, TraceContextExt};
+use opentelemetry::trace::{SpanKind, TraceContextExt};
 use opentelemetry::{global, Context};
 use opentelemetry::{
     propagation::Extractor,
@@ -88,14 +87,13 @@ pub trait Tracing {
         span.set_parent(child_cx.clone());
         (span, child_cx)
     }
-    fn start_child_otel_context<'a>(
-        parent_cx: &'a opentelemetry::Context,
+    fn start_child_otel_context(
+        parent_cx: &opentelemetry::Context,
         app_name: &'static str,
         span_name: String,
     ) -> Context {
         let child_otel_span = global::tracer(app_name).start_with_context(span_name, parent_cx);
-        let child_cx = parent_cx.with_span(child_otel_span);
-        child_cx
+        parent_cx.with_span(child_otel_span)
     }
     fn trace_request<T: Debug>(
         name: &'static str,
